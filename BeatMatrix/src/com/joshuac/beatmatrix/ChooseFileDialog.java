6249946d2247aad22e4917fdbe0840519c4b3a94
Matt Harrison <harrisonm1@ufl.edu>
2013-03-24 16:37:17 -0400

    File selection changed to only includes .wav files
    MyAD only supports .wav files. We will have to revert or change some
    code if we need .mp3 support

--- BeatMatrix/src/com/joshuac/beatmatrix/ChooseFileDialog.java
+++ BeatMatrix/src/com/joshuac/beatmatrix/ChooseFileDialog.java
@@ -64,7 +64,7 @@ public class ChooseFileDialog extends DialogFragment {
 					int periodIndex= file.toString().lastIndexOf('.');
 					if(periodIndex != -1){
 						String ext = file.toString().substring(periodIndex);
-						if(ext.equals(".mp3")||ext.equals(".wav")){
+						if(ext.equals(".wav")){
 								files.add(file);
 						}//if try-catch
 					}
